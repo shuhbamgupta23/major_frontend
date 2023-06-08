30e955e27ce96058b02b519d4e74a758592827df
unknown <mrjanuaryx@gmail.com>
2023-06-08 19:00:52 +0530

    first commit

--- src/component/Product/Products.js
+++ src/component/Product/Products.js
@@ -39,7 +39,7 @@ const Products = ({ match }) => {
     resultPerPage,
     filteredProductsCount,
   } = useSelector((state) => state.products);
-
+  console.log(products)
   const keyword = match.params.keyword;
 
   const setCurrentPageNo = (e) => {
